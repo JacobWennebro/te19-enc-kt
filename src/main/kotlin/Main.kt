989a5562d789badad772196c6a58c86067fb3cd7
JacobWennebro <jacob@wennebro.com>
2022-02-03 13:39:02 +0100

    Made XOR encryption work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,11 +8,32 @@ fun numberGenerator(iterations: Int): IntArray {
     return numbers
 }
 
+fun encrypt(msg: String, key: String): String {
+    val chars: List<Char> = msg.toList();
+    val keyChars: List<Char> = key.toList();
+
+    val keyXor = IntArray(keyChars.size);
+
+    for(i in 0 until keyChars.size) keyXor.set(i, keyChars.get(i).toInt());
+
+    var response = "";
+
+    for(i in 0 until chars.size) {
+        val charCode = chars.get(i).toInt();
+        response += charCode.xor(keyXor.get(i)).toChar();
+    }
+
+    return response;
+}
+
+fun decrypt(hash: String, key: String): String {
+    return encrypt(hash, key);
+}
+
+
 fun main(args: Array<String>) {
-    val numbers = numberGenerator(1000);
 
-    println(numbers.get(999))
-    println(numbers.get(929))
-    println(numbers.get(99))
+    val res = encrypt("leeråy", "boopip");
 
+    println(decrypt(res, "boopip"));
 }
